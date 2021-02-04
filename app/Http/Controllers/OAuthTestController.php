eb3fd36ad713efd628ab5e876a70519ea86f19a4
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-05 00:34:49 +0900

    auth入れたりした

--- app/Http/Controllers/OAuthTestController.php
+++ app/Http/Controllers/OAuthTestController.php
@@ -8,6 +8,21 @@ use Laravel\Socialite\Facades\Socialite;
 class OAuthTestController extends Controller
 {
     public function test() {
-        return Socialite::driver('google')->redirect()->getTargetUrl();
+        return Socialite::driver('google')->redirect();
+    }
+    public function handleProviderCallback($provider = 'google') {
+        try {
+            $providerUser = Socialite::with('provider')->user();
+            $email = $providerUser->getEmail() ?? null;
+            $providerId = $providerUser->getId();
+            // if($email) {
+
+            // }
+
+            return redirect("http://localhost:8080");
+        } catch(\Exception $e) {
+            $message = "authentication failed !!";
+            return redirect("http://localhost:8080");
+        }
     }
 }
