09cd0b9911e52f448878b0057b22b0171d1000ea
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-05 18:54:27 +0900

    tokenをvueに渡せるようになった

--- app/Http/Controllers/OAuthTestController.php
+++ app/Http/Controllers/OAuthTestController.php
@@ -8,20 +8,20 @@ use Laravel\Socialite\Facades\Socialite;
 class OAuthTestController extends Controller
 {
     public function test() {
-        return Socialite::driver('google')->redirect();
+        // return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')->redirect()->getTargetUrl();
     }
     public function handleProviderCallback($provider) {
+        // $providerUser = Socialite::with($provider)->user();
+        $providerUser = Socialite::with($provider)->stateless()->user(); // Laravel\Socialite\Two\InvalidStateException のエラーを解決するためにはこうしないといけないらしい
 
         try {
-            $providerUser = Socialite::with($provider)->user();
             $email = $providerUser->getEmail() ?? null;
             $providerId = $providerUser->getId();
 
-            // return redirect("http://localhost:8080/#".$providerUser->token);
-            $msg = $providerUser->token;
-            return view('debug', compact('msg'));
+            return redirect("http://localhost:8080/#".$providerUser->token);
         } catch(\Exception $e) {
-            $msg = "authentication failed !!";
+            $msg = "認証に失敗しました。\n元のページに戻ってください。";
             return view('debug', compact('msg'));
         }
     }
